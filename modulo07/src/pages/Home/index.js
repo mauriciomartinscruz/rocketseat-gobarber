e6d6882c268a9876889ccc3963df7417e0de3d7a
Mauricio Martins <mauriciomartinscruz@gmail.com>
2020-04-20 18:57:43 -0300

    finished layout

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,88 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from "react-icons/md";
+
+import api from '../../services/api';
+import {formatPrice} from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
+export default class Home extends Component {
+
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    this.setState({ products: data })
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="1#FFF" /> 3
           </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer fsdf sd fad fa dfasd fas dfasd fasdf asd fasd fasd fasdfasd</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe1.jpg?ts=1587358092?ims=280x280" alt="itens" />
-        <strong>Tenis title herer</strong>
-        <span>R$ 9,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="1#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))};
+      </ProductList>
+    );
+  }
 }
