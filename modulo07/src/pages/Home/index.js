74ed952fc47ddfbfe5240ad5d14848ccfc033e4f
Mauricio Martins <mauriciomartinscruz@gmail.com>
2020-04-21 21:47:56 -0300

    Add cart in redux

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import { MdAddShoppingCart } from "react-icons/md";
 
 import api from '../../services/api';
@@ -6,7 +7,7 @@ import {formatPrice} from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
 
   state = {
     products: [],
@@ -23,6 +24,15 @@ export default class Home extends Component {
     this.setState({ products: data })
   }
 
+  handleAddProduct = product => {
+    const {dispatch} = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   render() {
     const { products } = this.state;
 
@@ -34,7 +44,7 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="1#FFF" /> 3
           </div>
@@ -46,3 +56,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
